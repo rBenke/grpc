a014ac73a18f103e694c59f2ab2fd9b7bece3072
rBenke <robert.benke2@gmail.com>
2020-11-07 19:09:53 +0100

    R grpc helloworld

---
+++ grpc_example/helloworld/greeter_client.R
@@ -0,0 +1,17 @@
+#' Example gRPC client
+#'
+#' Sends a message with a name and returns a message greeting the name.
+#' @references \url{https://github.com/grpc/grpc/tree/master/examples/cpp/helloworld}
+
+library(grpc)
+
+
+spec <- system.file('examples/helloworld.proto', package = 'grpc')  
+impl <- read_services(spec)
+client <- grpc_client(impl, "localhost:50051")
+
+hello <- client$HelloRequest$build(name='you')
+message <- client$SayHello$call(hello)
+
+print(message)
+print(as.list(message))
